0cd26a0dd690bb725c7ae91530c8fe90690b2ed8
abmbodj <vriistt@gmail.com>
2026-03-16 20:16:52 -0400

    chore: Delete the `riven-social` project, various documentation, and internal notes.

--- riven-social/src/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { loadFont as loadCormorant } from '@remotion/google-fonts/CormorantGaramond';
-import { loadFont as loadLora } from '@remotion/google-fonts/Lora';
-import { loadFont as loadInter } from '@remotion/google-fonts/Inter';
-import { loadFont as loadJetBrainsMono } from '@remotion/google-fonts/JetBrainsMono';
-
-export const { fontFamily: cormorant } = loadCormorant();
-export const { fontFamily: lora } = loadLora();
-export const { fontFamily: inter } = loadInter();
-export const { fontFamily: jetbrains } = loadJetBrainsMono();
